9bbb8768e0a8afabf4f27d6dd33df2e7ae016ba8
JKTUNING <jktuned.crypto@gmail.com>
2022-08-21 12:12:01 -0400

    remove cdn-changes

--- cdn-speedtest.sh
+++ cdn-speedtest.sh
@@ -20,16 +20,6 @@ HOT="${ORANGE}\xF0\x9F\x94\xA5${NC}"
 WORNING="${RED}\xF0\x9F\x9A\xA8${NC}"
 RIGHT_ANGLE="${GREEN}\xE2\x88\x9F${NC}"
 
-#allow user input for download time between 4 and 10 seconds - might help stabilize connection
-if [ -z "$1" ]; then
-  downloadtime='4'
-elif [[ $1 -lt 10 && $1 -ge 4 ]]; then
-  downloadtime="$1"
-else
-    downloadtime='4'
-fi
-
-#array of available bootstrap server IDs
 rand_by_domain=("5" "6" "7" "8" "9" "10" "11" "12")
 size_list=()
 i=0
@@ -38,10 +28,9 @@ echo -e ""
 echo -e "${YELLOW}Running quick download speed test for flux_explorer_bootstrap...${NC}"
 while [ $i -lt $len ];
 do
-    #test download speed for -m $downloadtime and output to testing
-    testing=$(curl -m $downloadtime http://cdn-${rand_by_domain[$i]}.runonflux.io/apps/fluxshare/getfile/flux_explorer_bootstrap.tar.gz  --output testspeed -fail --silent --show-error 2>&1)
+    testing=$(curl -m 4 http://cdn-${rand_by_domain[$i]}.runonflux.io/apps/fluxshare/getfile/flux_explorer_bootstrap.tar.gz  --output testspeed -fail --silent --show-error 2>&1)
     testing_size=$(grep -Po "\d+" <<< "$testing" | paste - - - - | awk '{printf  "%d\n",$3}')
-    mb=$(bc <<<"scale=2; $testing_size / 1048576 / ($downloadtime)" | awk '{printf "%2.2f\n", $1}')
+    mb=$(bc <<<"scale=2; $testing_size / 1048576 / 4" | awk '{printf "%2.2f\n", $1}')
     echo -e "   ${RIGHT_ANGLE} ${GREEN}cdn-${YELLOW}${rand_by_domain[$i]}${GREEN} - Bits Downloaded: ${YELLOW}$testing_size${NC} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
     size_list+=($testing_size)
     i=$(($i+1))
@@ -56,7 +45,7 @@ for i in "${!size_list[@]}"; do
 done
 
 # Print the results
-mb=$(bc <<<"scale=2; $arr_max / 1048576 / ($downloadtime)" | awk '{printf "%2.2f\n", $1}')
+mb=$(bc <<<"scale=2; $arr_max / 1048576 / 4" | awk '{printf "%2.2f\n", $1}')
 echo -e ""
 echo -e "${YELLOW}Best server is: ${GREEN}cdn-${YELLOW}${rand_by_domain[${max_indexes[0]}]} ${GREEN}Average speed: ${YELLOW}$mb ${GREEN}MB/s${NC}"
 echo -e "${CHECK_MARK} ${GREEN}Fastest Server: ${YELLOW}http://cdn-${rand_by_domain[${max_indexes[@]}]}.runonflux.io/apps/fluxshare/getfile/flux_explorer_bootstrap.tar.gz${NC}"
